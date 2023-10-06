f8a448d158d5b25471d1566b49c75a3ff064aa08
Amos Brocco <amos.brocco@gmail.com>
2023-10-06 16:34:51 +0200

    6.10.2023 - Fine

--- S3/main2.cpp
+++ S3/main2.cpp
@@ -6,14 +6,37 @@ Fraction operator+(Fraction a, const Fraction& b)
     return a += b;
 }
 
+std::ostream& operator<<(std::ostream& o, const Fraction& f) {
+    o << f.getNumerator() << "/" << f.getDenominator();
+    return o;
+}
+
+
+/*Fraction operator+(const Fraction& a, const Fraction& b)
+{
+    Fraction c{a};
+    c += b;
+    return c;
+}*/
+
+
 void stampa(const Fraction& f) {
     std::cout << f.getNumerator() << "/" << f.getDenominator() << '\n';
 }
 
 int main() {
     Fraction f{3,4};
+    Fraction g{3,4};
     f.print();
 
-    f = 7;
+    f = Fraction{7};
     stampa(f);
+
+    f += g;
+    f.operator+=(g);
+
+
+    std::cout << f;
+    g = f + 5; // g = f.operator+(5); -> f.operator+(Fraction{5});
+    //g = 5 + f; // g = 5.operator+(f);
 }
